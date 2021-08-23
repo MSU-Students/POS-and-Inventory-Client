39b0b91571dc5cda13ce52279f9c6227034b6a79
Serad, Basam C <58251967+DeJaVu1223@users.noreply.github.com>
2021-08-23 16:09:52 +0800

    Login UI

--- src/pages/ManageAccount.vue
+++ src/pages/ManageAccount.vue
@@ -6,15 +6,7 @@
           <div class="text-h6">Our Changing Planet</div>
           <div class="text-subtitle2">by John Doe</div>
         </q-card-section>
-
-        <q-card-section> </q-card-section>
-
         <q-separator dark />
-
-        <q-card-actions>
-          <q-btn flat>Action 1</q-btn>
-          <q-btn flat>Action 2</q-btn>
-        </q-card-actions>
       </q-card>
       <q-card class="my-card bg-secondary text-white">
         <q-card-section>
