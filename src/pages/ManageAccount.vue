076cb1d68302f643bb8f22c611b4e2c73a4eba13
Serad, Basam C <58251967+DeJaVu1223@users.noreply.github.com>
2021-08-31 09:52:53 +0800

    update

--- src/pages/ManageAccount.vue
+++ src/pages/ManageAccount.vue
@@ -41,7 +41,7 @@
             icon="add"
             @click="addUser = true"
           />
-          <q-dialog v-model="addUser">
+          <q-dialog v-model="addUser" persistent>
             <q-card style="width: 350px">
               <q-card-section class="row">
                 <div class="text-h6">Add User</div>
@@ -72,102 +72,88 @@
           </q-dialog>
         </div>
       </template>
-      <template v-slot:header="props">
-        <q-tr :props="props">
-          <q-th auto-width />
-          <q-th v-for="col in props.cols" :key="col.name" :props="props">
-            {{ col.label }}
-          </q-th>
-        </q-tr>
-      </template>
 
-      <template v-slot:body="props">
-        <q-tr :props="props">
-          <div>
-            <q-td>
-              <q-btn
-                round
-                color="blue"
-                icon="edit"
-                size="sm"
-                flat
-                dense
-                @click="editRow = true"
-              />
-              <q-dialog v-model="editRow">
-                <q-card style="width: 350px">
-                  <q-card-section class="row">
-                    <div class="text-h6">Edit User</div>
-                    <q-space />
-                    <q-btn flat round dense icon="close" v-close-popup />
-                  </q-card-section>
+      <template v-slot:body-cell-action="props">
+        <q-td :props="props">
+          <div class="q-gutter-sm">
+            <q-btn
+              round
+              color="blue"
+              icon="edit"
+              size="sm"
+              flat
+              dense
+              @click="editRow = true"
+            />
+            <q-dialog v-model="editRow" persistent>
+              <q-card style="width: 350px">
+                <q-card-section class="row">
+                  <div class="text-h6">Edit User</div>
+                  <q-space />
+                  <q-btn flat round dense icon="close" v-close-popup />
+                </q-card-section>
 
-                  <q-card-section class="q-gutter-md">
-                    <q-input outlined v-model="name" label="First Name" />
-                    <q-input outlined v-model="name" label="Middle Initial" />
-                    <q-input outlined v-model="name" label="Last Name" />
-                    <q-input outlined v-model="username" label="Username" />
-                    <q-input outlined v-model="password" label="Password" />
-                    <q-input
-                      outlined
-                      v-model="email"
-                      label="Email"
-                      type="email"
-                    />
-                    <q-select
-                      outlined
-                      v-model="role"
-                      :options="options"
-                      label="Roles"
-                    />
-                  </q-card-section>
+                <q-card-section class="q-gutter-md">
+                  <q-input outlined v-model="name" label="First Name" />
+                  <q-input outlined v-model="name" label="Middle Initial" />
+                  <q-input outlined v-model="name" label="Last Name" />
+                  <q-input outlined v-model="username" label="Username" />
+                  <q-input outlined v-model="password" label="Password" />
+                  <q-input
+                    outlined
+                    v-model="email"
+                    label="Email"
+                    type="email"
+                  />
+                  <q-select
+                    outlined
+                    v-model="role"
+                    :options="options"
+                    label="Roles"
+                  />
+                </q-card-section>
 
-                  <q-card-actions align="right">
-                    <q-btn flat label="Cancel" color="red-10" v-close-popup />
-                    <q-btn flat label="Save" color="primary" v-close-popup />
-                  </q-card-actions>
-                </q-card>
-              </q-dialog>
-              <q-btn
-                color="red-10"
-                icon="delete"
-                size="sm"
-                class="q-ml-sm"
-                flat
-                round
-                dense
-                @click="dialog = true"
-              />
-              <q-dialog v-model="dialog" persistent>
-                <q-card style="width: 300px">
-                  <q-card-section class="row items-center">
-                    <q-avatar
-                      size="sm"
-                      icon="warning"
-                      color="red-10"
-                      text-color="white"
-                    />
-                    <span class="q-ml-sm">Confirm Delete?</span>
-                  </q-card-section>
-                  <q-card-actions align="right">
-                    <q-btn
-                      flat
-                      label="Cancel"
-                      color="primary"
-                      v-close-popup="cancelEnabled"
-                      :disable="!cancelEnabled"
-                    />
-                    <q-btn flat label="Confirm" color="primary" v-close-popup />
-                  </q-card-actions>
-                </q-card>
-              </q-dialog>
-            </q-td>
+                <q-card-actions align="right">
+                  <q-btn flat label="Cancel" color="red-10" v-close-popup />
+                  <q-btn flat label="Save" color="primary" v-close-popup />
+                </q-card-actions>
+              </q-card>
+            </q-dialog>
+            <q-btn
+              color="red-10"
+              icon="delete"
+              size="sm"
+              class="q-ml-sm"
+              flat
+              round
+              dense
+              @click="dialog = true"
+            />
+            <q-dialog v-model="dialog" persistent>
+              <q-card style="width: 300px">
+                <q-card-section class="row items-center">
+                  <q-avatar
+                    size="sm"
+                    icon="warning"
+                    color="red-10"
+                    text-color="white"
+                  />
+                  <span class="q-ml-sm">Confirm Delete?</span>
+                </q-card-section>
+                <q-card-actions align="right">
+                  <q-btn
+                    flat
+                    label="Cancel"
+                    color="primary"
+                    v-close-popup="cancelEnabled"
+                    :disable="!cancelEnabled"
+                  />
+                  <q-btn flat label="Confirm" color="primary" v-close-popup />
+                </q-card-actions>
+              </q-card>
+            </q-dialog>
           </div>
-
-          <q-td v-for="col in props.cols" :key="col.name" :props="props">
-            {{ col.value }}
-          </q-td>
-        </q-tr>
+        </q-td>
       </template>
     </q-table>
   </div>
@@ -208,6 +194,7 @@ export default class ManageAccount extends Vue {
       label: 'Last Login',
       field: 'lastLogin',
     },
+    { name: 'action', align: 'center', label: 'Action', field: 'action' },
   ];
   rows = [
     {
