641a0ec24321710f0ed2cdb8f16bff42f51ed8e4
ZhyneAmenodin <amenodin.za28@s.msumain.edu.ph>
2021-10-22 14:26:56 +0800

    Extension

---
+++ src/pages/SalesReport.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="q-pa-md">
+    <div class="col">
+        <div class="text-h4 q-pb-lg text-bold row">
+            <q-icon name="payments" color="indigo" style="font-size: 4rem" />
+            Sales report
+        </div>
+        <div>
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
+
+    </div>
+</template>
