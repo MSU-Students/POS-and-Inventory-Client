e1e2e229da6e160c51cc7ea77db112657a7cd39a
Serad, Basam C <58251967+DeJaVu1223@users.noreply.github.com>
2021-10-25 12:24:38 +0800

    Dashboard and Changes

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="q-pa-md">
+    <div class="row q-gutter-lg">
+      <div class="col">
+        <q-card align="center">
+          <q-card-section>
+            <q-icon
+              name="signal_cellular_alt"
+              color="deep-purple"
+              style="font-size: 3rem"
+            >
+            </q-icon>
+            <div class="text-h5 text-deep-purple q-pb-md">Revenue</div>
+            <div class="text-h5">10000.000</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card align="center">
+          <q-card-section>
+            <q-icon name="shopping_cart" color="amber" style="font-size: 3rem">
+            </q-icon>
+            <div class="text-h5 text-amber q-pb-md">Purchase</div>
+            <div class="text-h5">10000.000</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card align="center">
+          <q-card-section>
+            <q-icon name="shopping_bag" color="teal" style="font-size: 3rem">
+            </q-icon>
+            <div class="text-h5 text-teal q-pb-md">Expenses</div>
+            <div class="text-h5">10000.000</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card align="center">
+          <q-card-section>
+            <q-icon name="paid" color="cyan" style="font-size: 3rem"> </q-icon>
+            <div class="text-h5 text-cyan q-pb-md">Profit</div>
+            <div class="text-h5">10000.000</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+@Options({})
+export default class Dashboard extends Vue {}
+</script>
--- src/pages/POS.vue
+++ src/pages/POS.vue
@@ -1,160 +1,158 @@
 <template>
-  <div class="q-pa-md ">
+  <div class="q-pa-md">
     <div class="text-h4 q-pb-lg text-bold row">
-        <q-icon name="point_of_sale" color="orange" style="font-size: 4rem" />
-        POS
+      <q-icon name="point_of_sale" color="orange" style="font-size: 4rem" />
+      POS
     </div>
-    <div class="row">
-    <div class="q-pa-md col-8">
-    <q-table
-      title="Menu"
-      :rows="rows"
-      :columns="columns"
-      row-key="name"
-      selection="multiple"
-      v-model:selected="selected"
-      :filter="filter"
-      grid
-      hide-header
-    >
-    
-      <template v-slot:top-right>
-        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
-          <template v-slot:append>
-            <q-icon name="search" />
-          </template>
-        </q-input>
-      </template>
-      <template v-slot:body-cell-action="props">
-        <q-td :props="props">
-            <q-badge :label="props.value"></q-badge>
-            <img :src="images[props.row.imageIndex].url"/>
-        </q-td> 
-      </template>
-    </q-table>
+    <div class="row q-gutter-md">
+      <div class="full-height col-8">
+        <q-card>
+          <div class="q-pa-md">
+            <q-table
+              title="Menu"
+              :rows="rows"
+              :columns="columns"
+              row-key="name"
+              :filter="filter"
+              grid
+              hide-header
+              hide-bottom
+            >
+              <template v-slot:top-right>
+                <q-input
+                  borderless
+                  dense
+                  debounce="300"
+                  v-model="filter"
+                  placeholder="Search"
+                >
+                  <template v-slot:append>
+                    <q-icon name="search" />
+                  </template>
+                </q-input>
+              </template>
+              <template v-slot:body-cell-action="props">
+                <q-td :props="props">
+                  <q-badge :label="props.value"></q-badge>
+                </q-td>
+              </template>
+            </q-table>
+          </div>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card>
+          <div>
+            <q-card flat bordered class="my-card">
+              <q-card-section>
+                <q-toolbar class="bg-primary text-white shadow-2">
+                  <q-toolbar-title>Selected</q-toolbar-title>
+                </q-toolbar>
+                <q-list dense bordered padding class="rounded-borders">
+                  <q-item>
+                    <q-item-section> Item </q-item-section>
+                  </q-item>
+
+                  <q-item>
+                    <q-item-section> Item </q-item-section>
+                  </q-item>
+
+                  <q-item>
+                    <q-item-section> Item </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-card-section>
+
+              <q-separator inset />
+
+              <q-card-section> Total: </q-card-section>
+            </q-card>
+          </div>
+        </q-card>
+      </div>
+    </div>
   </div>
-   <div class="col-4" >
-      <q-card flat bordered class="my-card">
-      <q-card-section>
-        <q-toolbar class="bg-primary text-white shadow-2">
-      <q-toolbar-title>Selected</q-toolbar-title>
-    </q-toolbar>
-    <q-list dense bordered padding class="rounded-borders">
-      <q-item>
-        <q-item-section>
-          Item
-        </q-item-section>
-      </q-item>
-
-      <q-item >
-        <q-item-section>
-          Item
-        </q-item-section>
-      </q-item>
-
-      <q-item>
-        <q-item-section>
-          Item
-        </q-item-section>
-      </q-item>
-    </q-list>
-      </q-card-section>
-
-      <q-separator inset />
-
-      <q-card-section>
-       Total:
-      </q-card-section>
-    </q-card>
- 
-    </div>
-    </div>
-  </div>
-  
 </template>
 
-<script>
-import { ref } from 'vue'
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
 
-const columns = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Dessert (100g serving)',
-    align: 'left',
-    field: row => row.name,
-    format: val => `${val}`,
-    sortable: true
-  },
-  
+interface IRow {
+  name: string;
+}
+
+@Options({})
+export default class POS extends Vue {
+  columns = [
+    {
+      name: 'name',
+      required: true,
+      label: 'Dessert (100g serving)',
+      align: 'left',
+      field: (row: IRow) => row.name,
+      format: (val: string) => `${val}`,
+      sortable: true,
+    },
+
     {
       name: 'price',
       align: 'price',
       label: 'price',
       field: 'price',
     },
-  { name: 'img', 
-    label: 'img', 
-    field: 'img', 
-    sortable: true, 
-    style: 'width: 10px' 
-  }
-]
+    {
+      name: 'img',
+      label: 'img',
+      field: 'img',
+      sortable: true,
+      style: 'width: 10px',
+    },
+  ];
 
-const rows = [
-  {
-    name: 'Frozen Yogurt',
-    price: 10
-    
-  },
-  {
-    name: 'Ice cream sandwich',
-    price: 10
-  },
-  {
-    name: 'Eclair',
-    price: 10
-  
-    
-  },
-  {
-    name: 'Cupcake',
-    price: 10
-  },
-  {
-    name: 'Gingerbread',
-    price: 10
-  },
-  {
-    name: 'Jelly bean',
-    price: 10
-  },
-  {
-    name: 'Lollipop',
-    price: 10
-  },
-  {
-    name: 'Honeycomb',
-    price: 10
-  },
-  {
-    name: 'Donut',
-    price: 10
-  },
-  {
-    name: 'KitKat',
-    price: 10
-  }
-]
+  rows = [
+    {
+      name: 'Frozen Yogurt',
+      price: 10,
+    },
+    {
+      name: 'Ice cream sandwich',
+      price: 10,
+    },
+    {
+      name: 'Eclair',
+      price: 10,
+    },
+    {
+      name: 'Cupcake',
+      price: 10,
+    },
+    {
+      name: 'Gingerbread',
+      price: 10,
+    },
+    {
+      name: 'Jelly bean',
+      price: 10,
+    },
+    {
+      name: 'Lollipop',
+      price: 10,
+    },
+    {
+      name: 'Honeycomb',
+      price: 10,
+    },
+    {
+      name: 'Donut',
+      price: 10,
+    },
+    {
+      name: 'KitKat',
+      price: 10,
+    },
+  ];
 
-export default {
-  setup () {
-    return {
-      filter: ref(''),
-      selected: ref([]),
-      columns,
-      rows
-    }
-  }
+  filter = '';
+  selected = [];
 }
 </script>
